fe6a364c7692511c4bb44dac8c89660b75625bec
NekesaH <Nyongesacharity13@gmail.com>
2022-07-26 07:59:03 +0300

    Images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import Logo from './img/logo.png'
 
 const Header = () => {
   return (
-  <div className='fixed w-screen z-50 bg-slate-300 p-6 px-16'>
+  <div className='fixed w-screen z-50 p-6 px-16'>
     {/*desktop & tablet */}
-    <div className="hidden md:flex w-full h-full bg-red-600 p-4"></div>
+    <div className="hidden md:flex w-full h-full">
+        <div className="flex items-center gap-2">
+            <img src={Logo} className="w-10 object-cover" alt="Logo" />
+
+        </div>
+    </div>
   
      {/* mobile*/}
-     <div className='block md:hidden w-full h-full bg-blue-600 p-4
+     <div className='block md:hidden w-full h-full p-4
      '></div>
 
   </div>
-  );
+  ); 
   
 };
 
